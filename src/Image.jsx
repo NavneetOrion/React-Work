0b255e3d85e26c8b99dff330efe5b75640e7898e
NavneetOrion <navneet.kumar@deeporion.com>
2023-04-19 18:09:12 +0530

    Adding Styles

---
+++ src/Image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Image2 from './Image/7.jpg'
+function Image(){
+    return(<>
+        <img src={Image2} alt="avatar" className="avatar" height={400} width={400}></img>
+    </>)
+}
+
+export default Image;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import AboutPage  from './AboutPage';
+import Image from './Image';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +10,7 @@ root.render(
   <>
     <App />
     <AboutPage/>
+    <Image/>
   </>
     
   
